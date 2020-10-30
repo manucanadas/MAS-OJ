334e11afe5f856868525b15759344b9a67abe614
Alex-Shatskiy <alexlev706@gmail.com>
2020-10-30 15:51:24 +1300

    work pLZ

--- dbFunctions.js
+++ dbFunctions.js
@@ -3,16 +3,12 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
-
-
 function getRegion (id, db = connection) {
   return db ('details')
   .join('locations' , 'details.location_id' , 'locations.id')
   .where('locations.location', id)
   .select()
 }
-
-
 
 module.exports = {
     getRegion
